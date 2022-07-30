33769994e54cc29ada9a3cd50397e92026509a69
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-07-30 17:24:06 -0300

    Commit rota delete

--- unyleya/src/main/java/projetobackend/unyleya/controller/FuncionarioController.java
+++ unyleya/src/main/java/projetobackend/unyleya/controller/FuncionarioController.java
@@ -4,11 +4,16 @@
  */
 package projetobackend.unyleya.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,4 +48,19 @@ public class FuncionarioController {
     public Funcionario creaFuncionario(@Valid @RequestBody Funcionario funcionario){
         return funcionarioRepository.save(funcionario);
     }
+    
+    @DeleteMapping("/funcionarios/{id}")
+    public ResponseEntity<Object> deleteFuncionario(@PathVariable(value="id") Long funcionarioId) 
+    {
+        Optional<Funcionario> funcionario = funcionarioRepository.findById(funcionarioId);
+        
+        if(funcionario.isPresent())
+        {
+            funcionarioRepository.delete(funcionario.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
