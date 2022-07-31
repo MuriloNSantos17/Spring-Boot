e84b07d2f85049292eb4a1fe885848ba4e378819
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-07-31 09:48:33 -0300

    Commit create project

---
+++ cidadaos/src/main/java/unyleya/example/cidadaos/CidadaosApplication.java
@@ -0,0 +1,13 @@
+package unyleya.example.cidadaos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadaosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadaosApplication.class, args);
+	}
+
+}
---
+++ cidadaos/src/test/java/unyleya/example/cidadaos/CidadaosApplicationTests.java
@@ -0,0 +1,13 @@
+package unyleya.example.cidadaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadaosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
