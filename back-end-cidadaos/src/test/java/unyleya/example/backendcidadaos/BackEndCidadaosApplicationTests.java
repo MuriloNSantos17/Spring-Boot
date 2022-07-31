f3d68e385c42cfe8962b335838911c934a94fdd0
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-07-31 09:59:46 -0300

    Commit novo projeto

---
+++ back-end-cidadaos/src/test/java/unyleya/example/backendcidadaos/BackEndCidadaosApplicationTests.java
@@ -0,0 +1,13 @@
+package unyleya.example.backendcidadaos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCidadaosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
