3c04d1db2c0df6cc9ba64056e58a386a76bbe5f2
MURILO NUNES DOS SANTOS <49132949+MuriloNSantos17@users.noreply.github.com>
2022-07-31 12:10:42 -0300

    Commit metodo post

--- back-end-cidadaos/src/main/java/unyleya/backend/cidadaos/controller/CidadadoController.java
+++ back-end-cidadaos/src/main/java/unyleya/backend/cidadaos/controller/CidadadoController.java
@@ -4,10 +4,14 @@
  */
 package unyleya.backend.cidadaos.controller;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import unyleya.backend.cidadaos.model.Cidadao;
 import unyleya.backend.respository.CidadaoRepository;
 
 
@@ -17,4 +21,9 @@ import unyleya.backend.respository.CidadaoRepository;
 public class CidadadoController {
     @Autowired
     private CidadaoRepository cidadaoRepository;
+    
+    @PostMapping("/cidadaos")
+    public Cidadao createCidadao(@Valid @RequestBody Cidadao cidadao){
+        return cidadaoRepository.save(cidadao);
+    }
 }
